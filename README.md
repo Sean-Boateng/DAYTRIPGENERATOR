ad553d05e34a2d5215fa1bfa1a8e3267f8e7e2ac
Sean <seanboateng2@gmail.com>
2022-10-21 16:46:17 -0400

    User can now select resturant at random

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # DAYTRIPGENERATOR
-
-
-(5 points): As a developer, I want to make at least three commits with descriptive messages
--- project1.py
+++ project1.py
@@ -16,6 +16,18 @@ entertainment = ["Movie Night", "Clubing/Partying", "Jet Skiing", "Dancing"]
 # (5 points): As a user, I want a destination to be randomly selected for my day trip.
 
 def place_to_go():
-    print(random.choice(destination))
+    print("You will be going to " +random.choice(destination))
 
 user_destination = place_to_go()
+
+
+#(5 points): As a user, I want a restaurant to be randomly selected for my day trip
+
+def where_to_eat():
+    print("Enjoy dining at " +random.choice(resturant))
+
+user_resturant = where_to_eat()
+
+
+
+
