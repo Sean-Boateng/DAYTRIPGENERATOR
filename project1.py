0ddb28c2895a4fb70a7017c45de5677a6d3c9583
Sean <seanboateng2@gmail.com>
2022-10-21 16:33:14 -0400

    User can now pick choose destination at random

--- project1.py
+++ project1.py
@@ -1,9 +1,21 @@
 # (5 points): As a developer, I want to make at least three commits with descriptive messages
 # (5 points):  As a developer, I want to store my destinations, restaurants, mode of transportation, and entertainment in their own separate Lists. 
 
+
+
+
+import random
+
+
 destination = ["Peru", "China", "Ghana", "England"]
 resturant = ["Los Amigos", "Wine 30", "Seafood", "Vegan"]
 mode_of_transport = ["Foot", "Bicycle", "Car", "Plane"]
 entertainment = ["Movie Night", "Clubing/Partying", "Jet Skiing", "Dancing"]
 
 
+# (5 points): As a user, I want a destination to be randomly selected for my day trip.
+
+def place_to_go():
+    print(random.choice(destination))
+
+user_destination = place_to_go()
