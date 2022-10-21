8adfac37b7eb435b8ec0ca50cc5a694d9866e2e6
Sean <seanboateng2@gmail.com>
2022-10-21 17:15:29 -0400

    User can now select an activity at random

--- project1.py
+++ project1.py
@@ -8,15 +8,15 @@ import random
 
 
 destination = ["Peru", "China", "Ghana", "England"]
-resturant = ["Los Amigos", "Wine 30", "Seafood", "Vegan"]
-mode_of_transport = ["Foot", "Bicycle", "Car", "Plane"]
-entertainment = ["Movie Night", "Clubing/Partying", "Jet Skiing", "Dancing"]
+resturant = ["Los Amigos", "Wine 30", "Ruth Chris", "PF Changs"]
+mode_of_transport = ["by Foot", " by Public Transportation", "by Car", " by Plane"]
+entertainment = ["a Movie Night", "Clubing/Partying", "Jet Skiing", "Dancing"]
 
 
 # (5 points): As a user, I want a destination to be randomly selected for my day trip.
 
 def place_to_go():
-    print("You will be going to " +random.choice(destination))
+    print("You will be going to " +random.choice(destination)+"!!")
 
 user_destination = place_to_go()
 
@@ -24,10 +24,26 @@ user_destination = place_to_go()
 #(5 points): As a user, I want a restaurant to be randomly selected for my day trip
 
 def where_to_eat():
-    print("Enjoy dining at " +random.choice(resturant))
+    print("Enjoy dining at " +random.choice(resturant)+"!")
 
 user_resturant = where_to_eat()
 
+
+# (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip. 
+
+def how_to_get_there():
+    print("Your primary mode of transportation,while on this trip, will be "+ random.choice(mode_of_transport))
+
+user_means_of_transport = how_to_get_there()
+
+
+# (5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.
+
+
+def what_to_do():
+    print("If you ever get bored, try "+ random.choice(entertainment)+" with friends")
+
+user_activity = what_to_do()
 
 
 
